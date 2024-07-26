b61260d1d4c2a07eaca96964ffcef96e1fbd924c
Bipin Sai Surya G <bss220904@gmail.com>
2024-07-26 18:57:31 +0530

    Second commit

--- CFG/website/views.py
+++ CFG/website/views.py
@@ -2,14 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login , logout
 from django.contrib import messages
 
-def home(request):
-
-	return render(request,'home.html',{})
-
-def new(request):
-	if request.method=='POST':
-		message = request.POST.get("message")
-		return render(request,'new.html',{'data':message})
 
 
 
